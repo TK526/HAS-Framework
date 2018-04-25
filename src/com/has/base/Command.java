ec069d9632de3cfefb1899abad6c4d0133ee7014
egeozanozyedek <36128615+egeozanozyedek@users.noreply.github.com>
2018-04-25 23:12:49 +0300

    Update Command.java

--- src/com/has/base/Command.java
+++ src/com/has/base/Command.java
@@ -1,6 +1,49 @@
 package com.has.base;
 
-public class Command
-{
-
+/*
+ * Command class which contains commandName and arguments
+ * @author Ege Ozan Özyedek
+ * @version v1.0
+ * @date 24/04/18
+ */
+public class Command {
+  
+  //properties
+  CommandName commandName;
+  DataContainer arguments;
+  
+  //constructors
+  
+  /*
+   * Command class constructor, gets commandName and arguments and holds them
+   * @param commandName, the name of the command
+   * @param arguments, DataConatiner which holds the arguments
+   */
+  public Command( CommandName commandName, DataContainer arguments) {
+    
+    this.commandName = commandName;
+    this.arguments = arguments;
+    
+  }
+  
+  //methods
+  
+  /*
+   * A method which returns the arguments of command
+   * @return arguments, the property of Command
+   */
+  public DataContainer getArgument() {
+    
+    return arguments;
+  }
+  
+  /*
+   * A method which returns the name of command
+   * @return commandName, the property of Command
+   */
+  public CommandName getName() {
+    
+    return commandName;
+  }
+  
 }
